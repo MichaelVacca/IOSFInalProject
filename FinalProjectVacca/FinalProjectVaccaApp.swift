f7aa8303974ea55674b01bb050ea6f2381e54da9
Michael Vacca <michaelvacca07@gmail.com>
2023-10-25 16:41:26 -0400

    Initial Commit

---
+++ FinalProjectVacca/FinalProjectVaccaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectVaccaApp.swift
+//  FinalProjectVacca
+//
+//  Created by macuser on 2023-10-25.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectVaccaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
