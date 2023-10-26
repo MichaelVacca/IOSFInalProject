f49d5e05e094ee44224ac1bee77a16c2e46e3c38
MichaelVacca <michaelvacca07@gmail.com>
2023-10-26 17:52:55 -0400

    Added File

--- FinalProjectVacca/View/BookDetails.swift
+++ FinalProjectVacca/View/BookDetails.swift
@@ -2,7 +2,17 @@
 //  BookDetails.swift
 //  FinalProjectVacca
 //
-//  Created by MACUSER on 2023-10-26.
+//  Created by Gabriella Vacca on 2023-10-26.
 //
 
-import Foundation
+import SwiftUI
+
+struct BookDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookDetails()
+}
