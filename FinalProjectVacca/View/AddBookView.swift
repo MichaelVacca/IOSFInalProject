b783f84d40eb1924b16c12c0e3344c4cfe97151a
MichaelVacca <michaelvacca07@gmail.com>
2023-10-26 17:53:59 -0400

    Full Commit

--- FinalProjectVacca/View/AddBookView.swift
+++ FinalProjectVacca/View/AddBookView.swift
@@ -7,11 +7,69 @@
 
 import Foundation
 import SwiftUI
+struct CustomTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color.blue, lineWidth: 2) // Adjust color and line width as needed
+            )
+    }
+}
 
-
-struct AddBook: View{
-    var body: some View{
-        Text("Add a new book here")
+struct AddBook: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var bookName = ""
+    @State private var author = ""
+    @State private var price = ""
+    @State private var genre = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                // Book Name
+                TextField("Book Name", text: $bookName)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .padding()
+                
+                // Author
+                TextField("Author", text: $author)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .padding()
+                
+                // Price
+                TextField("Price", text: $price)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .padding()
+                
+                // Genre
+                TextField("Genre", text: $genre)
+                    .textFieldStyle(CustomTextFieldStyle())
+                    .padding()
+                
+                // Button to Add the Book
+                Button("Add Book") {
+                    // Save the book to your database or perform other actions here
+                    // Example:
+                    saveBookToDatabase()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+                .padding()
+            }
+            .navigationBarTitle("Add Book")
+        }
+    }
+    
+    // Function to save the book to the database
+    func saveBookToDatabase() {
+        // Implement your code to save the book to your database here
+        
+        // After saving, dismiss the AddBook view and navigate back to the previous view
+        self.presentationMode.wrappedValue.dismiss()
     }
 }
 
--- FinalProjectVacca/View/BookDetails.swift
+++ FinalProjectVacca/View/BookDetails.swift
@@ -7,12 +7,86 @@
 
 import SwiftUI
 
-struct BookDetails: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+import SwiftUI
 
-#Preview {
-    BookDetails()
+struct BookDetails: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var bookName = ""
+    @State private var author = ""
+    @State private var price = ""
+    @State private var genre = ""
+    @State private var dataAdded = ""
+    @State private var amountInInventory = ""
+    @State private var date = Date()
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [Color.yellow, Color.green]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Update Book")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .position(x: UIScreen.main.bounds.width / 2, y: 10)
+                    .padding([.top, .leading, .trailing])
+                
+                VStack {
+                    // Book Name
+                    TextField("Book Name", text: $bookName)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        .padding()
+                    
+                    // Author
+                    TextField("Author", text: $author)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        .padding()
+                    
+                    // Price
+                    TextField("Price", text: $price)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        .padding()
+                    
+                    // Genre
+                    TextField("Genre", text: $genre)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        .padding()
+                    
+                    DatePicker("Date Added", selection: $date, displayedComponents: [.date])
+                        .padding(.top, -15)
+                    
+                    TextField("Amount in Inventory", text: $amountInInventory)
+                        .textFieldStyle(CustomTextFieldStyle())
+                        .padding()
+                    
+                    // Button to Add the Book
+                    HStack {
+                        Button("Update Book") {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .padding()
+                        
+                        Button("Delete Book") {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                        .padding()
+                    }
+                    
+                    Button("Back") {
+                        self.presentationMode.wrappedValue.dismiss()
+                    }
+                }
+            }
+        }
+        .navigationBarBackButtonHidden(true) // This hides the back button
+        .navigationBarHidden(true)
+    }
 }
